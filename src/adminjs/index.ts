406414e1185e0c1db288989429326c393887ac3f
edilson madsen <edilson_madsen@hotmail.com>
2023-01-11 00:41:12 -0300

    eadinspiradoNetflix

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -1,8 +1,12 @@
+import { dashboardOptions } from "./dashboard";
+import { locale } from "./locale";
 import AdminJs from "adminjs";
 import AdminJsExpress from "@adminjs/express";
 import AdminJsSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
+import { brandingOptions } from "./branding";
+import { authtenticationOptions } from "./authentication";
 
 AdminJs.registerAdapter(AdminJsSequelize);
 
@@ -10,27 +14,17 @@ export const adminJs = new AdminJs({
   databases: [sequelize],
   rootPath: "/admin",
   resources: adminJsResources,
-  branding: {
-    companyName: "OneBitFlix",
-    logo: "/onebitflix.svg",
-    theme: {
-      colors: {
-        primary100: "#ff0043",
-        primary80: "#ff1a57",
-        primary60: "#ff3369",
-        primary40: "#ff4d7c",
-        primary20: "#ff668f",
-        grey100: "#151515",
-        grey80: "#333333",
-        grey60: "#4d4d4d",
-        grey40: "#666666",
-        grey20: "#dddddd",
-        filterBg: "#333333",
-        accent: "#151515",
-        hoverBg: "#151515",
-      },
-    },
-  },
+  branding: brandingOptions,
+  locale: locale,
+  dashboard: dashboardOptions,
 });
 
-export const adminJsRouter = AdminJsExpress.buildRouter(adminJs);
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
+  adminJs,
+  authtenticationOptions,
+  null,
+  {
+    resave: false,
+    saveUninitialized: false,
+  }
+);
